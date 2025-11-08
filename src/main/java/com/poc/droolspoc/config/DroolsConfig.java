31c936617402778182b4644d90b0832ac0174428
edgarrth <edgarrt@gmail.com>
2025-11-08 11:11:32 -0500

    Add files via upload

---
+++ src/main/java/com/poc/droolspoc/config/DroolsConfig.java
@@ -0,0 +1,16 @@
+package com.poc.droolspoc.config;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+class DroolsConfig {
+
+    /*@Bean
+    public KieContainer kieContainer() {
+        KieServices kieServices = KieServices.Factory.get();
+        return kieServices.getKieClasspathContainer();
+    }*/
+}
